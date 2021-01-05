f6ecfa24e550787e811d967bc910c67199f8fbca
agustin <aguszorza@gmail.com>
2021-01-05 16:29:24 +0100

    User endpoint

--- app/user/routes.py
+++ app/user/routes.py
@@ -3,7 +3,8 @@ from flask import Blueprint, jsonify
 user_route = Blueprint('user', __name__)
 
 
-@user_route.route('/user', methods=['GET'])
-def get_carousel():
+@user_route.route('/user/<user_id>', methods=['GET'])
+def get_carousel(user_id):
     # TODO: replace it with a class and a request to the database (only if we need a user in the project)
+    print(user_id)
     return jsonify({'full_name': 'Helder Betiol'}), 200
