63c090468bc230c7094b29c8ff418d900f7c7f78
agustin <aguszorza@gmail.com>
2021-01-05 16:34:12 +0100

    small changes

--- app/user/routes.py
+++ app/user/routes.py
@@ -4,7 +4,6 @@ user_route = Blueprint('user', __name__)
 
 
 @user_route.route('/user/<user_id>', methods=['GET'])
-def get_carousel(user_id):
+def get_user(user_id):
     # TODO: replace it with a class and a request to the database (only if we need a user in the project)
-    print(user_id)
     return jsonify({'full_name': 'Helder Betiol'}), 200
